acda76a0fb4c9543873b3386257421c9f5dbd4ba
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-12-17 14:12:24 +0200

    changed loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,17 @@
-import { Triangle } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={css.background}>
-      <Triangle
-        visible={true}
-        height="120"
-        width="120"
-        color="#0081CF"
-        ariaLabel="triangle-loading"
+      <Circles
+        height="80"
+        width="80"
+        color="#85aa9f"
+        ariaLabel="circles-loading"
         wrapperStyle={{}}
         wrapperClass={css.wrapper}
+        visible={true}
       />
     </div>
   );
